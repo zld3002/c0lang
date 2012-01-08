6a97070f7e31163b5bb50fe57fa733e77c92f6d1
Frank Pfenning <fp@cs.cmu.edu>
2012-01-08 23:44:05 +0000

    changed configure and makefile; added missing args.c
    git-svn-id: https://svn.concert.cs.cmu.edu/c0@18 59102d7d-8565-4dee-862f-f79749aaa1f2

--- cc0/doc/reference/README.txt
+++ cc0/doc/reference/README.txt
@@ -1,9 +1,9 @@
 c0-reference.pdf
 
 % pdflatex c0-reference.tex
-% scp c0-reference.pdf ...
+% scp c0-reference.pdf c0.typesafety.net:/home/www/c0/doc/
 
 c0-libraries.pdf
 
 % pdflatex c0-libraries.tex
-% scp c0-libraries.pdf ...
+% scp c0-libraries.pdf c0.typesafety.net:/home/www/c0/doc/
---
+++ cc0/libs/args/args.c
@@ -0,0 +1,134 @@
+#include <string.h>
+#include <c0runtime.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+#include <assert.h>
+
+/* lib/cc0main.c defined these */
+extern int c0_argc;
+extern char **c0_argv;
+
+enum args_type {
+  ARGS_BOOL, ARGS_INT, ARGS_STRING
+};
+
+struct args_list {
+  const char *name;
+  enum args_type tp;
+  void *ptr;
+  struct args_list *next;
+};
+
+struct args_list *args_list = NULL;
+
+void args_flag(c0_string name, bool *ptr) {
+  assert(ptr != NULL);
+  struct args_list *l = c0_alloc(sizeof(struct args_list));
+  l->name = c0_string_tocstr(name);
+  l->tp = ARGS_BOOL;
+  l->ptr = (void*) ptr;
+  l->next = args_list;
+  args_list = l;
+}
+
+void args_int(c0_string name, int *ptr) {
+  assert(ptr != NULL);
+  struct args_list *l = c0_alloc(sizeof(struct args_list));
+  l->name = c0_string_tocstr(name);
+  l->tp = ARGS_INT;
+  l->ptr = (void*) ptr;
+  l->next = args_list;
+  args_list = l;
+}
+
+void args_string(c0_string name, c0_string *ptr) {
+  assert(ptr != NULL);
+  struct args_list *l = c0_alloc(sizeof(struct args_list));
+  l->name = c0_string_tocstr(name);
+  l->tp = ARGS_STRING;
+  l->ptr = (void*) ptr;
+  l->next = args_list;
+  args_list = l;
+}
+
+struct args_list *args_get(const char *name) {
+  struct args_list *l = args_list;
+  while (l != NULL) {
+    if (!strcmp(l->name, name))
+      return l;
+    l = l->next;
+  }
+  return NULL;
+}
+
+struct args {
+  int argc;
+  c0_array* argv;		/* string[] */
+};
+typedef struct args* args_t;
+
+int get_int (const char *arg) {
+  errno = 0;
+  char *endptr;
+  long int li = strtol(arg, &endptr, 10);
+  if (errno == 0 && !isspace(*arg) && *arg != '+' /* don't allow leading space or + */
+      && INT_MIN <= li && li <= INT_MAX		  /* ensure in range for int */
+      && endptr != arg && *endptr == '\0') { /* ensure whole string is parsed */
+    return (int)li;
+  } else {
+    fprintf(stderr, "\"%s\" is not a valid integer\n", arg);
+    c0_abort("invalid command line argument");
+  }
+}
+
+args_t args_parse() {
+  int argc = c0_argc-1;		/* set in main */
+  char **argv = c0_argv+1;	/* set in main */
+  int other_count = 0;
+  struct args_list* other_list = NULL;
+  while (argc > 0) {
+    struct args_list *l = args_get(*argv);
+    if (l == NULL) {
+      /* not a recognized argument - add to "others" */
+      other_count++;
+      struct args_list* o = c0_alloc(sizeof(struct args_list));
+      o->name = *argv;
+      o->tp = ARGS_STRING;
+      /* o->ptr = NULL; */ /* unused */
+      o->next = other_list;
+      other_list = o;
+      argc--; argv++;
+    } else {
+      argc--; argv++;
+      switch (l->tp) {
+      case ARGS_BOOL:
+	*(bool*)l->ptr = true;
+	break;
+      case ARGS_INT:
+	*(int*)l->ptr = get_int(*argv);
+	argc--; argv++;
+	break;
+      case ARGS_STRING:
+	*(c0_string*)l->ptr = c0_string_fromliteral(*argv);
+	argc--; argv++;
+	break;
+      }
+    }
+  }
+
+  c0_array* args_array = c0_array_alloc(sizeof(c0_string), other_count);
+  /* reverse linked list order to obtain command line order */
+  for (int i = other_count-1; 0 <= i; i--) {
+    *(c0_string*)c0_array_sub(args_array, i, sizeof(c0_string)) =
+      c0_string_fromcstr(other_list->name);
+    other_list = other_list->next;
+  }
+
+  struct args *args = (struct args *)c0_alloc(sizeof(struct args));
+  args->argc = other_count;
+  args->argv = args_array;
+
+  return args;
+}
