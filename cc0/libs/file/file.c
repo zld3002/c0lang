bceb2a7908cadb3ec248398ac16049372ddfbb2a
Carsten Varming <cvarming@cs.cmu.edu>
2012-11-27 17:59:42 +0000

    Fixed file.c to handle subsequent call on closed files
    git-svn-id: https://svn.concert.cs.cmu.edu/c0@151 59102d7d-8565-4dee-862f-f79749aaa1f2

--- cc0/libs/file/file.c
+++ cc0/libs/file/file.c
@@ -13,22 +13,25 @@ file_t file_read(c0_string path) {
   const char* filename = c0_string_tocstr(path);
   file_t f = c0_alloc(sizeof(struct file));
   f->handle = fopen(filename, "r");
-  if (!f->handle) {
-    return NULL;
-  }
+  if (!f->handle) return NULL;
   return f;
 }
 
 void file_close(file_t f) {
   assert(f != NULL);
+  if (f->handle == NULL) 
+    c0_abort("file_close: file already closed");
   if (EOF == fclose(f->handle)) {
     c0_abort("Could not close file");
   }
+  f->handle = NULL;
   return;
 }
 
 bool file_eof(file_t f) {
   assert(f != NULL);
+  if (f->handle == NULL) 
+    c0_abort("file_eof: file is closed");
   if (EOF == fgetc(f->handle)) {
     return true;
   }
@@ -38,6 +41,8 @@ bool file_eof(file_t f) {
 
 c0_string file_readline(file_t f) {
   assert(f != NULL);
+  if (f->handle == NULL) 
+    c0_abort("file_readline: file is closed");
   if (feof(f->handle) || file_eof(f)) {
     c0_abort("Cannot read more lines - already at end of file");
   }
