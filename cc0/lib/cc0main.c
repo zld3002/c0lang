61996659acb78fe40661012d1f5ee9766adca879
Frank Pfenning <fp@cs.cmu.edu>
2012-12-27 18:09:58 +0000

    added check for two's complement arithmetic in cc0main.c
    git-svn-id: https://svn.concert.cs.cmu.edu/c0@220 59102d7d-8565-4dee-862f-f79749aaa1f2

--- cc0/lib/cc0main.c
+++ cc0/lib/cc0main.c
@@ -8,6 +8,7 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <c0runtime.h>  /* for c0_runtime_init(), c0_runtime_cleanup() */
 
 int _c0_main();
@@ -42,9 +43,33 @@ void really_fclose(FILE *stream, char *error)
   }
 }
 
+int int_max() {
+  return INT_MAX;
+}
+
+int int_min() {
+  return INT_MIN;
+}
+
 int main(int argc, char **argv) {
   c0_argc = argc;
   c0_argv = argv;
+
+  /* static_assert is not part of C99 standard */
+  /* use manual, runtime checks to verify machine parameters */
+  /* gcc must be called with -fwrapv for correct implementation
+   * of two's complement arithmetic prescribed in C0
+   */
+  if (sizeof(int) != 4) {
+    fprintf(stderr, "Error: sizeof(int) == %zd != 4\n", sizeof(int));
+    exit(1);
+  }
+  if (~(-1) != 0 || int_max()+1 != int_min() || int_min()-1 != int_max()) {
+    fprintf(stderr, "Error: not a two's complement machine\n");
+  }
+  if ((-1 >> 31) != -1) {
+    fprintf(stderr, "Error: arithmetic right shift does not sign-extend\n");
+  }
 
   char *filename = getenv("C0_RESULT_FILE");
 
