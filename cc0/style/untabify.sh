38af44d6154cf6f04e2207a8bf7effed3289f13b
Frank Pfenning <fp@cs.cmu.edu>
2013-08-21 20:11:39 +0000

    small bug fixes and improvements; added -t n flag to untabify
    git-svn-id: https://svn.concert.cs.cmu.edu/c0@332 59102d7d-8565-4dee-862f-f79749aaa1f2

--- cc0/style/untabify.sh
+++ cc0/style/untabify.sh
@@ -1,14 +1,19 @@
 #!/bin/sh
-# usage: untabify.sh filename
+# usage: untabify.sh [-t n] file1 [file2 ...]
 
-if [ $# -eq 0 ]
-    then
+if [ $# -eq 0 ]; then
     echo "usage: $0 file1 [file2 ...]"
     exit 1;
 fi
+if [ $1 = '-t' ]; then
+    tabstop=$2
+    shift 2
+else 
+    tabstop=8
+fi
 for f in $*; do
     tmpfile=$f.$$
-    untabify < $f > $tmpfile
+    untabify-exec $tabstop < $f > $tmpfile
     if [ $? -eq 0 ]; then
         mv $tmpfile $f
         if [ $? -ne 0 ]; then
